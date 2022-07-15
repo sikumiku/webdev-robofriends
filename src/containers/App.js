1103bc0f257e67c60d344abd98e439512fa798e1
sikumiku <sigridaasma@gmail.com>
2022-07-15 03:25:16 +0300

    start using redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
+import { setSearchField } from '../actions';
 
-function App() {
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+function App(props) {
 
     const [robots, setRobots] = useState([]);
-    const [searchField, setSearchField] = useState('');
+    const { searchField, onSearchChange } = props;
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users));
     },[])
-
-    const onSearchChange = (event) => {
-        setSearchField(event.target.value);
-    }
 
     const filteredBots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
@@ -39,4 +49,4 @@ function App() {
             );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App/>    
+      <Provider store={store}>
+        <App /> 
+      </Provider>   
   </React.StrictMode>
 );
 
